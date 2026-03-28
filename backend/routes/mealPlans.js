4763307396e71a7bd5faf69d1afa395f6a186f14
TamarawGuy <viktor.m.rumenov@gmail.com>
2026-03-28 16:57:33 +0200

    Feat: meal plan router

---
+++ backend/routes/mealPlans.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import * as mealPlanController from '../controllers/mealPlanController.js'
+import authMiddleware from '../middleware/auth'
+
+const router = express.Router()
+
+// All routes are protected
+router.use(authMiddleware)
+
+router.get('/weekly', mealPlanController.getWeeklyMealPlan)
+router.get('/upcoming', mealPlanController.getUpcomingMeals)
+router.get('/stats', mealPlanController.getMealPlanStats)
+router.post('/', mealPlanController.addToMealPlan)
+router.delete('/:id', mealPlanController.deleteMealPlan)
+
+export default router
