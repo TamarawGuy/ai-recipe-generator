364d8cd05183b56ac1ac46e1042a9559970fa665
TamarawGuy <viktor.m.rumenov@gmail.com>
2026-03-28 16:28:06 +0200

    Feat: meal plan controller

---
+++ backend/controllers/mealPlanController.js
@@ -0,0 +1,101 @@
+import MealPlan from '../models/MealPlan'
+
+/**
+ * Add recipe to meal plan
+ */
+export const addToMealPlan = async (req, res, next) => {
+    try {
+        const mealPlan = await MealPlan.create(req.user.id, req.body)
+
+        res.status(201).json({
+            success: true,
+            message: 'Recipe added to meal plan',
+            data: { mealPlan },
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+/**
+ * Get weekly meal plan
+ */
+export const getWeeklyMealPlan = async (req, res, next) => {
+    try {
+        const { start_date, weekStartDate } = req.query
+        const startDate = start_date || weekStartDate
+
+        if (!startDate) {
+            return res.status(400).json({
+                success: false,
+                message: 'Please provide start_date or weekStartDate',
+            })
+        }
+
+        const mealPlans = await MealPlan.getWeeklyPlan(req.user.id, startDate)
+        res.json({
+            success: false,
+            data: { mealPlans },
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+/**
+ * Get upcoming meals
+ */
+export const getUpcomingMeals = async (req, res, next) => {
+    try {
+        const limit = Number(req.query.limit) || 5
+        const meals = await MealPlan.getUpcoming(req.user.id, limit)
+
+        res.json({
+            success: true,
+            data: { meals },
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+/**
+ * Delete meal plan
+ */
+export const deleteMealPlan = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const mealPlan = await MealPlan.delete(id, req.user.id)
+
+        if (!mealPlan) {
+            res.status(404).json({
+                success: false,
+                message: 'Meal plan not found',
+            })
+        }
+
+        res.json({
+            success: true,
+            message: 'Meal plan entry deleted',
+            data: { mealPlan },
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+/**
+ * Get meal plan stats
+ */
+export const getMealPlanStats = async (req, res, next) => {
+    try {
+        const stats = await MealPlan.getStats(req.user.id)
+
+        res.json({
+            success: true,
+            data: { stats },
+        })
+    } catch (err) {
+        next(err)
+    }
+}
